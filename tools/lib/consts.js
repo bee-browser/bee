7a0a7321230823667edeb00f0f67dd15922516f5
masnagam <masnagam@gmail.com>
2023-04-16 22:13:05 +0900

    build(deps): update dependencies

--- tools/lib/consts.js
+++ tools/lib/consts.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import * as path from 'https://deno.land/std@0.181.0/path/mod.ts';
+import * as path from 'https://deno.land/std@0.183.0/path/mod.ts';
 
 const DIRNAME = path.dirname(path.fromFileUrl(import.meta.url));
 
