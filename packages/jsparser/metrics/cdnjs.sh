5a7185a6036bdff896498e522a5a51f519441901
masnagam <masnagam@gmail.com>
2023-08-28 17:17:30 +0900

    test(jsparser): ignore broken links

--- packages/jsparser/metrics/cdnjs.sh
+++ packages/jsparser/metrics/cdnjs.sh
@@ -11,23 +11,27 @@ TOTAL=$(curl https://api.cdnjs.com/libraries?fields=fileType -sG | jq -c "$FILTE
 COUNT=1
 for LIB in $(curl https://api.cdnjs.com/libraries?fields=fileType -sG | jq -c "$FILTER")
 do
-  NAME=$(echo "$LIB" | jq -r '.name')
   URL=$(echo "$LIB" | jq -r '.latest')
-  METRICS=$(curl "$URL" -sG | cargo run -r -q --example=bee-jsparser-demo 2>/dev/null)
-  if [ "$?" = 0 ]
+  if ! curl "$URL" -sGf >/dev/null
   then
-    echo "$COUNT/$TOTAL: + $URL"
-    TIME=$(echo "$METRICS" | cut -d ' ' -f 1 | cut -d '=' -f 2)
-    SIZE=$(echo "$METRICS" | cut -d ' ' -f 2 | cut -d '=' -f 2)
-    DEPTH=$(echo "$METRICS" | cut -d ' ' -f 3 | cut -d '=' -f 2)
-    cat <<EOF >>$OUTPUT
+    echo "$COUNT/$TOTAL: ! $URL"
+  else
+    METRICS=$(curl "$URL" -sG | cargo run -r -q --example=bee-jsparser-demo 2>/dev/null)
+    if [ "$?" = 0 ]
+    then
+      echo "$COUNT/$TOTAL: + $URL"
+      TIME=$(echo "$METRICS" | cut -d ' ' -f 1 | cut -d '=' -f 2)
+      SIZE=$(echo "$METRICS" | cut -d ' ' -f 2 | cut -d '=' -f 2)
+      DEPTH=$(echo "$METRICS" | cut -d ' ' -f 3 | cut -d '=' -f 2)
+      cat <<EOF >>$OUTPUT
 {"url":"$URL","parsed":true,"time":$TIME,"size":$SIZE,"maxStackDepth":$DEPTH}
 EOF
-  else
-    echo "$COUNT/$TOTAL: - $URL"
-    cat <<EOF >>$OUTPUT
+    else
+      echo "$COUNT/$TOTAL: - $URL"
+      cat <<EOF >>$OUTPUT
 {"url":"$URL","parsed":false}
 EOF
+    fi
   fi
   COUNT=$(expr $COUNT + 1)
 done
