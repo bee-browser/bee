ac499149bd7fcae5a858249b179cbdcb295507a9
masnagam <masnagam@gmail.com>
2023-10-04 10:56:39 +0900

    build(deps): update dependencies

--- tools/bin/codegen.js
+++ tools/bin/codegen.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import * as fs from 'https://deno.land/std@0.202.0/fs/mod.ts';
-import * as path from 'https://deno.land/std@0.202.0/path/mod.ts';
+import * as fs from 'https://deno.land/std@0.203.0/fs/mod.ts';
+import * as path from 'https://deno.land/std@0.203.0/path/mod.ts';
 import * as changeCase from 'https://deno.land/x/case@2.2.0/mod.ts';
 import Handlebars from 'npm:handlebars@4.7.8'
 import { parseCommand, readAllText } from '../lib/cli.js';
