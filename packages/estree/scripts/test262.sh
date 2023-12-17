cbfe8f927c82479730bc4fe6a7f8dd623db9ca6f
masnagam <masnagam@gmail.com>
2023-12-17 11:34:52 +0900

    ci: realpath doesn't exist in the macos runner

--- packages/estree/scripts/test262.sh
+++ packages/estree/scripts/test262.sh
@@ -1,4 +1,4 @@
-BASE_DIR=$(realpath $(dirname $0))
-PROJ_DIR=$(realpath $BASE_DIR/../../..)
+BASE_DIR=$(cd $(dirname $0); pwd)
+PROJ_DIR=$(cd $BASE_DIR/../../..; pwd)
 TEST262_DIR=$PROJ_DIR/vendor/tc39/test262
 deno run --allow-read --allow-run=cargo $BASE_DIR/test262.js $@
