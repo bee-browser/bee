39027964fca54eec9fba6dc325f89d6b81c190fe
masnagam <masnagam@gmail.com>
2024-01-02 12:42:34 +0900

    fix(estree): specify the package

--- bins/estree/scripts/flamegraph.sh
+++ bins/estree/scripts/flamegraph.sh
@@ -3,4 +3,4 @@ SAVED=$(cat /proc/sys/kernel/perf_event_paranoid)
 trap "echo $SAVED | sudo tee /proc/sys/kernel/perf_event_paranoid >/dev/null" EXIT
 
 echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid >/dev/null
-cat | cargo flamegraph --profile=profiling -- parse module >/dev/null
+cat | cargo flamegraph -p estree --profile=profiling -- parse module >/dev/null
