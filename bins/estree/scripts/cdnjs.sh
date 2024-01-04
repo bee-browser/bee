46d72afa3138939a6cbfa9a54b3f76e73e788958
masnagam <masnagam@gmail.com>
2024-01-04 10:32:55 +0900

    fix(estree): webkit.js consumes a lot of memory

--- bins/estree/scripts/cdnjs.sh
+++ bins/estree/scripts/cdnjs.sh
@@ -1,6 +1,8 @@
 BASE_DIR=$(cd $(dirname $0); pwd)
+# webkit.js consumes a lot of memory...
 deno run -q \
   --allow-net=deno.land,api.cdnjs.com,cdnjs.cloudflare.com \
   --allow-run=cargo \
   --allow-read=$BASE_DIR \
+  --v8-flags='--max-old-space-size=2000' \
   $BASE_DIR/cdnjs.js $@
--- bins/estree/scripts/validate.sh
+++ bins/estree/scripts/validate.sh
@@ -1,2 +1,6 @@
 BASE_DIR=$(cd $(dirname $0); pwd)
-deno run -q --allow-run=cargo $BASE_DIR/validate.js $@
+# webkit.js consumes a lot of memory...
+deno run -q \
+  --allow-run=cargo \
+  --v8-flags='--max-old-space-size=2000' \
+  $BASE_DIR/validate.js $@
