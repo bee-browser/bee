52036d1a774a0f8a135c848b388ae48b2f79fdbf
masnagam <masnagam@gmail.com>
2024-02-21 09:36:13 +0900

    build(deps): update dependencies

--- tools/bin/serve_webui.js
+++ tools/bin/serve_webui.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import * as path from 'https://deno.land/std@0.214.0/path/mod.ts';
+import * as path from 'https://deno.land/std@0.216.0/path/mod.ts';
 import { parseCommand, runCommand } from '../lib/cli.js';
 import * as webui from '../lib/webui.js';
 
