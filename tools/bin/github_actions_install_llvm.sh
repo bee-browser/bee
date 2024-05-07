258e342e780c3ee9c938f16a4b75c565097e078b
masnagam <masnagam@gmail.com>
2024-05-07 22:27:47 +0900

    ci: install packages needed for LLVM

--- tools/bin/github_actions_install_llvm.sh
+++ tools/bin/github_actions_install_llvm.sh
@@ -4,6 +4,9 @@ wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 18
 
+# Install packages needed for LLVM to work.
+sudo apt-get install -y zstd
+
 # //libs/jsruntime/build.sh uses //vendor/bin/llvm-config.
 mkdir -p vendor/bin
 ln -sf $(which llvm-config-18) vendor/bin/llvm-config
