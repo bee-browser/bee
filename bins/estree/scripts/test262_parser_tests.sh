a3d582e5c9ebc081e7df0a9c782a648ca7655a55
masnagam <masnagam@gmail.com>
2024-06-15 13:57:29 +0900

    build(deps): update ora

--- bins/estree/scripts/test262_parser_tests.sh
+++ bins/estree/scripts/test262_parser_tests.sh
@@ -2,6 +2,6 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 PROJ_DIR=$(cd $BASE_DIR/../../..; pwd)
 deno run -q \
   --allow-env \
-  --allow-run=cargo \
+  --allow-run \
   --allow-read=$PROJ_DIR \
   $BASE_DIR/test262_parser_tests.js $@
