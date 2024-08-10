e482128bec9b666bd59eea5b37dbb73a16e115b0
masnagam <masnagam@gmail.com>
2024-08-10 21:01:23 +0900

    test(jsruntime): fix evaluate_try_throw_in_block_catch.js

--- libs/jsruntime/tests/evaluate_try_throw_in_block_catch.js
+++ libs/jsruntime/tests/evaluate_try_throw_in_block_catch.js
@@ -1,9 +1,15 @@
 try {
   let i = 0;
   {
+    print(i); ///=0
     throw () => i;
   }
+  // never reach here
   i = 1;
+  print(i);
 } catch (e) {
-  throw e() + 1; ///=1
+  print(e()); ///=0
+  throw e() + 1; ///!1
 }
+// never reach here
+print(100);
