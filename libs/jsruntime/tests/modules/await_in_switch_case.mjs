7971b6fee154b527ca0668fca69bb7334a038a08
masnagam <masnagam@gmail.com>
2024-10-20 10:44:46 +0900

    test(jsruntime): add test cases

---
+++ libs/jsruntime/tests/modules/await_in_switch_case.mjs
@@ -0,0 +1,12 @@
+print(0); ///#0=0
+switch (await 0) {
+  case await 0:
+    print(1); ///#1=1
+    await 0;
+    print(2); ///#2=2
+    break;
+  default:
+    print(100);
+    break;
+}
+print(3); ///#3=3
