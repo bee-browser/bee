4d11b220346b9a59ca81608f2e1685775ffe0e7e
masnagam <masnagam@gmail.com>
2025-03-05 15:54:33 +0900

    refactor(jsruntime): add wrapper types

--- libs/jsruntime/src/backend/cranelift/mod.rs
+++ libs/jsruntime/src/backend/cranelift/mod.rs
@@ -1,3 +1,13 @@
+use super::RuntimeFunctions;
+
 pub fn initialize() {
     // TODO
+}
+
+pub struct Executor;
+
+impl Executor {
+    pub fn new(_functions: &RuntimeFunctions) -> Self {
+        Self
+    }
 }
