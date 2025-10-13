dad63584d2848e442679eee77e8743a594adebaa
masnagam <masnagam@gmail.com>
2025-10-13 14:04:03 +0900

    refactor(jsruntime): generate mod.rs

---
+++ libs/jsruntime/src/objects/builtins/imp.js
@@ -0,0 +1,178 @@
+'use strict';
+
+import * as log from '@std/log';
+import * as path from '@std/path';
+import { TextLineStream, toTransformStream } from '@std/streams';
+import { constantCase } from 'change-case';
+import { JSDOM } from 'jsdom';
+import { parseCommand } from '../../../../../tools/lib/cli.js';
+import { VENDOR_DIR } from '../../../../../tools/lib/consts.js';
+import { setup } from '../../../../../tools/lib/log.js';
+
+const PROGNAME = path.basename(path.fromFileUrl(import.meta.url));
+const ECMA262_SPEC_HTML = path.join(VENDOR_DIR, 'src', 'tc39', 'ecma262', 'spec.html');
+
+const DOC = `
+Usage:
+  ${PROGNAME} [<imp.rs>]
+  ${PROGNAME} -h | --help
+
+Options:
+  -d, --debug
+    Enable debug logs.
+`;
+
+const { options, args } = await parseCommand({
+  doc: DOC,
+});
+
+if (options.debug) {
+  setup(PROGNAME, 'DEBUG');
+} else {
+  setup(PROGNAME, 'INFO');
+}
+
+Deno.exit(await main(args, options));
+
+async function main(args, options) {
+  log.debug(`Loading ${ECMA262_SPEC_HTML}...`);
+  const spec = new JSDOM(await Deno.readTextFile(ECMA262_SPEC_HTML));
+
+  const json = {
+    template: 'libs/jsruntime/src/object/builtins/string/mod.rs.mustache',
+    constructorProperties: {
+      functions: [],
+    },
+    prototypeProperties: {
+      functions: [],
+    },
+  };
+
+  for await (const data of dataStream(args.impRs)) {
+    collectDataFromSpec(spec, data);
+    switch (data.kind) {
+      case 'constructor.function':
+        json.constructorProperties.functions.push(data);
+        break;
+      case 'prototype.function':
+        json.prototypeProperties.functions.push(data);
+        break;
+    }
+  }
+
+  console.log(JSON.stringify(json));
+}
+
+function dataStream(impRs) {
+  function parseId(line) {
+    const parts = line.substring(3).split(' ');
+    return {
+      id: parts[0],
+      kind: parts[1],
+      options: parts.slice(2),
+    };
+  }
+
+  function parseImp(line) {
+    const parts = line.split(' ');
+    return parts[2].split('<')[0];
+  }
+
+  let stream = Deno.stdin;
+  if (impRs !== null) {
+    stream = Deno.open(impRs);
+  }
+
+  return stream
+    .readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream())
+    .pipeThrough(toTransformStream(async function* (lines) {
+      let state = 'id';
+      let data;
+      for await (let line of lines) {
+        line = line.trim();
+        if (line.length === 0) {
+          continue;
+        }
+        switch (state) {
+          case 'id':
+            if (!line.startsWith('//#')) {
+              continue;
+            }
+            data = parseId(line);
+            state = 'imp';
+            break;
+          case 'imp':
+            if (!line.startsWith('pub fn ')) {
+              continue;
+            }
+            data.imp = parseImp(line);
+            state = 'id';
+            yield data;
+            data = undefined;
+            break;
+        }
+      }
+    }));
+}
+
+function collectDataFromSpec(spec, data) {
+  let clause = spec.window.document.getElementById(data.id);
+  data.signature = parseSignature(clause.firstElementChild.textContent.trim());
+  data.alg = clause.getElementsByTagName('emu-alg').item(0).textContent;
+  data.length = 0;
+  for (const arg of data.signature.args) {
+    if (!arg.optional) {
+      data.length += 1;
+    }
+  }
+  switch (data.kind) {
+    case 'constructor.function':
+      data.name = data.signature.name.split('.')[1];
+      data.symbol = constantCase(data.signature.name.split('.')[1]);
+      break;
+    case 'prototype.function':
+      data.name = data.signature.name.split('.')[2];
+      data.symbol = constantCase(data.signature.name.split('.')[2]);
+      break;
+  }
+  return data;
+}
+
+function parseSignature(text, data) {
+  let parts = text.split('(');
+  let name = parts[0].trim();
+  let args = parseArgs(parts[1].split(')')[0].trim());
+  return { name, args };
+}
+
+function parseArgs(text) {
+  let args = [];
+  let optional = false;
+  let rest = false;
+  for (const part of text.split(' ')) {
+    switch (part) {
+      case '[':
+        optional = true;
+        break;
+      case ']':
+        optional = false;
+        break;
+      case ',':
+        break;
+      case '...':
+        rest = true;
+        break;
+      default:
+        args.push({
+          name: part.substring(1, part.length - 1), // remove '_'
+          optional,
+          rest,
+        });
+        rest = false;
+        break;
+    }
+  }
+  return args;
+}
