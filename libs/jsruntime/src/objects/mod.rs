3c9b73a7adfa6c1ad555977e754323f2f4453849
masnagam <masnagam@gmail.com>
2025-11-30 13:35:28 +0900

    refactor(jsruntime): rename: nucleus -> userdata

--- libs/jsruntime/src/objects/mod.rs
+++ libs/jsruntime/src/objects/mod.rs
@@ -215,11 +215,11 @@ impl PropertyFlags {
 // We use a simple hash map until we finishes implementing built-in objects.  After than, we'll
 // start reconsidering about the memory layout.
 pub struct Object {
-    /// An opaque value representing the *nucleus* of the object.
+    /// An opaque value of the object.
     ///
     /// A pointer to the `Closure` if this is a function object.
     /// A string handle if this is a string object.
-    nucleus: usize,
+    userdata: usize,
 
     flags: ObjectFlags,
 
@@ -229,12 +229,12 @@ pub struct Object {
 }
 
 impl Object {
-    pub(crate) const NUCLEUS_OFFSET: usize = std::mem::offset_of!(Self, nucleus);
+    pub(crate) const NUCLEUS_OFFSET: usize = std::mem::offset_of!(Self, userdata);
     pub(crate) const FLAGS_OFFSET: usize = std::mem::offset_of!(Self, flags);
 
     pub fn new(prototype: Option<ObjectHandle>) -> Self {
         Self {
-            nucleus: 0,
+            userdata: 0,
             flags: ObjectFlags::empty(),
             prototype,
             properties: Default::default(),
@@ -290,16 +290,21 @@ impl Object {
     }
 
     pub(crate) fn userdata(&self) -> usize {
-        self.nucleus
+        self.userdata
     }
 
     pub(crate) fn set_closure(&mut self, closure: *mut Closure) {
-        self.nucleus = closure.addr();
+        self.userdata = closure.addr();
         self.set_callable();
     }
 
+    pub(crate) fn string(&self) -> StringHandle {
+        // SAFETY: `self.userdata` is non-null and convertible to a reference.
+        unsafe { StringHandle::from_addr(self.userdata) }
+    }
+
     pub(crate) fn set_string(&mut self, string: StringHandle) {
-        self.nucleus = string.as_addr();
+        self.userdata = string.as_addr();
     }
 
     pub fn as_handle(&mut self) -> ObjectHandle {
